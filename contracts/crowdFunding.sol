1db0e44eb2e83bfe7714897a283f7d6aa7543581
MiAnClGr <MiAnClGr@gmail.com>
2022-07-31 17:00:17 +1000

    Updates to contract layout

--- contracts/crowdFunding.sol
+++ contracts/crowdFunding.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.0;
 
  contract crowdFunding {
+
+// State Variables
+    
     mapping(address => uint) public contributors;
+    
     address public admin;
+    
     uint public noOfContributors;
     uint public minimumContribution;
     uint public deadline; //timestamp
@@ -25,26 +30,37 @@ pragma solidity ^0.8.0;
     mapping(uint => Request) public requests;
     uint public numRequests;
     
-  
+// Events
+
     event ContributeEvent(address _sender, uint _value);
     event CreateRequestEvent(string _description, address _recipient, uint _value);
     event MakePaymentEvent(address _recipient, uint _value);
+
+// Modifiers
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can execute this");
+        _;
+    } 
     
-    
+// Constructor
+
     constructor(uint _goal, uint _deadline) {
         goal = _goal;
         deadline = block.timestamp + _deadline;
         admin = msg.sender;
         minimumContribution = 100 wei;
     }
-    
-   
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Only admin can execute this");
-        _;
+
+
+// Receive fallback function
+
+    receive() payable external {
+        contribute();
     }
     
-    
+// Contribute to the campaign
+
     function contribute() public payable {
         require(block.timestamp < deadline, "The Deadline has passed!");
         require(msg.value >= minimumContribution, "The minimum Contribution was not met!");
@@ -58,10 +74,8 @@ pragma solidity ^0.8.0;
         
         emit ContributeEvent(msg.sender, msg.value);
     }
-    
-    function getBalance() public view returns(uint) {
-        return address(this).balance;
-    }
+
+// Issues a refund to a contributor providing the deadline has passed 
     
     function getRefund() public {
         require(block.timestamp > deadline, "Deadline has not passed.");
@@ -74,6 +88,8 @@ pragma solidity ^0.8.0;
         contributors[msg.sender] = 0;  
         recipient.transfer(value);
     }
+
+// create a spending request
       
     function createRequest(string calldata _description, address payable _recipient, uint _value) public onlyAdmin {
         
@@ -88,8 +104,9 @@ pragma solidity ^0.8.0;
         
         emit CreateRequestEvent(_description, _recipient, _value);
     }
-    
-    
+
+// Vote for a particular spending request
+
     function voteRequest(uint _requestNo) public {
         require(contributors[msg.sender] > 0, "You must be a contributor to vote!");
         
@@ -100,7 +117,7 @@ pragma solidity ^0.8.0;
         thisRequest.noOfVoters++;
     }
     
-    
+// Completes the spending request given more than 50% of the votes
     function makePayment(uint _requestNo) public onlyAdmin {
         Request storage thisRequest = requests[_requestNo];
         require(thisRequest.completed == false, "The request has been already completed!");  
@@ -111,6 +128,8 @@ pragma solidity ^0.8.0;
         
         emit MakePaymentEvent(thisRequest.recipient, thisRequest.value);
     }  
+
+// Returns the request as individual variables (for front end access)
 
     function getRequest(uint _requestNo) public view returns(
         string memory description,
@@ -132,8 +151,9 @@ pragma solidity ^0.8.0;
                     );
     }
 
-    receive() payable external {
-        contribute();
-    }
+// Returns the balance of the contract
 
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
 }
